35597a57fc263485f5f0e3bf2f8d3b89687fff9b
John <634408262@qq.com>
2018-05-30 01:06:48 +0800

    add router-link

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/HelloWorld',
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 });

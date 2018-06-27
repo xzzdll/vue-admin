43aa4334b9ed59ef3573b3a705143cfdd662c6e2
John <634408262@qq.com>
2018-06-27 12:07:48 +0800

    增加水印

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="main-body" style="height:800px;margin:auto 0;position:fixed;width:calc(100% - 200px);min-width:800px;left:200px;top:61px;padding:20px">
+    <water-mack></water-mack>
   </div>
 </template>
 
 <script>
-import {fetch} from '../fetch/api.js';
+import { fetch } from '../fetch/api.js';
+import waterMack from '@/components/waterMack';
 export default {
   data () {
     return {};
+  },
+  components: {
+    waterMack
   },
   beforeCreate: function () {
     console.log('我将要被创建');
@@ -20,11 +25,13 @@ export default {
   },
   mounted: function () {
     console.log('我被挂载了');
-    fetch('/api/data').then((res, rej) => {
-      console.log(res);
-    }).catch((rej) => {
-      console.log(rej);
-    });
+    fetch('/api/data')
+      .then((res, rej) => {
+        console.log(res);
+      })
+      .catch(rej => {
+        console.log(rej);
+      });
   },
   beforeUpdate: function () {
     console.log('我将要被更新');
---
+++ src/components/waterMack.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class='waterMack'>
+    <div class='waterMackContent'>
+      <img v-bind:src=[imgUrl]>
+      <p>朱跃林 &nbsp; 2018/6/27</p>
+    </div>
+    <div class='waterMackContent'>
+      <img v-bind:src=[imgUrl]>
+      <p>朱跃林 &nbsp; 2018/6/27</p>
+    </div>
+    <div class='waterMackContent'>
+      <img v-bind:src=[imgUrl]>
+      <p>朱跃林 &nbsp; 2018/6/27</p>
+    </div>
+    <div class='waterMackContent'>
+      <img v-bind:src=[imgUrl]>
+      <p>朱跃林 &nbsp; 2018/6/27</p>
+    </div>
+    <div class='waterMackContent'>
+      <img v-bind:src=[imgUrl]>
+      <p>朱跃林 &nbsp; 2018/6/27</p>
+    </div>
+    <div class='waterMackContent'>
+      <img v-bind:src=[imgUrl]>
+      <p>朱跃林 &nbsp; 2018/6/27</p>
+    </div>
+    <div class='waterMackContent'>
+      <img v-bind:src=[imgUrl]>
+      <p>朱跃林 &nbsp; 2018/6/27</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      imgUrl: 'https://connect.zto.com/oauth2/static/zto-express.c3f90932.png'
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.waterMack {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  min-height: 100%;
+  height: 250px;
+  overflow: hidden;
+  pointer-events: none;
+  user-select: none;
+  opacity: 0.4;
+  z-index: 99999;
+}
+
+.waterMackContent {
+  float: left;
+  padding-top: 90px;
+  width: 300px;
+  height: 180px;
+  transform: rotate(-30deg);
+  margin: 50px 47.666666666666664px;
+}
+
+img {
+  width: 120px;
+  float: left;
+}
+
+p {
+  margin: 0px;
+  margin-left: 3%;
+  font-style: italic;
+  font-family: sans-serif;
+  color: #bbbbbb;
+}
+</style>

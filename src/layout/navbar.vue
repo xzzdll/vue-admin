326eb734df0147179347b3cd80cfabbd7c300fb7
John <634408262@qq.com>
2018-07-30 11:25:55 +0800

    update

--- src/layout/navbar.vue
+++ src/layout/navbar.vue
@@ -1,6 +1,12 @@
 <template>
   <div style="margin:auto 0;position:fixed;width:100%;min-width:800px;top:0px;background-color: #048cff;height:32px;color:#fff;">
-    <span class="header-left">{{curDate}}  登陆人：John</span>
+    <span class="header-left">{{curDate}} 登陆人：John</span>
+    <span class="header-right">
+      <a>
+        <i class="fa fa-sign-out" style="font-size: 14px;"></i>
+        退出
+      </a>
+    </span>
   </div>
 </template>
 
@@ -26,8 +32,15 @@ export default {
 
 <style lang="scss" scoped>
 .header-left {
+  float: left;
   height: 100%;
   line-height: 32px;
   margin-left: 15px;
+}
+.header-right {
+  float: right;
+  height: 100%;
+  line-height: 32px;
+  margin-right: 15px;
 }
 </style>

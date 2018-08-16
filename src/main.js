6aaf52c1351da313360945e0542688d00b1c3793
John <634408262@qq.com>
2018-08-16 13:48:48 +0800

    添加 quill依赖

--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ import router from './router';
 import Vuex from 'vuex';
 import store from './vuex/store';
 import ElementUI from 'element-ui';
+import VueQuillEditor from 'vue-quill-editor';
+import 'quill/dist/quill.core.css';
+import 'quill/dist/quill.snow.css';
+import 'quill/dist/quill.bubble.css';
 
+Vue.use(VueQuillEditor);
 Vue.use(Vuex);
 Vue.use(ElementUI);
 

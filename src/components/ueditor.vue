0e0d2670aa931af771c9caebadff3b7cec04240e
John <634408262@qq.com>
2018-08-16 11:36:47 +0800

    update

--- src/components/ueditor.vue
+++ src/components/ueditor.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    <script id="ueid" type="text/plain" style="width:1000px;height:240px;"></script>
+    <script id="ueid" type="text/plain"></script>
   </div>
 </template>
 <script>
+import '../assets/UE/third-party/jquery.min.js';
+import '../assets/UE/umeditor.config.js';
+import '../assets/UE/umeditor.min.js';
+import '../assets/UE/lang/zh-cn/zh-cn.js';
+import '../assets/UE/themes/default/css/umeditor.css';
+
 export default {
   name: 'UE',
   data () {
@@ -21,10 +27,12 @@ export default {
 
   },
   mounted () {
-    const _this = this;
-    this.editor = UM.getEditor('ueid', this.config); // 初始化UE
-    this.editor.addListener('ready', function () {
-      _this.editor.setContent(_this.defaultMsg); // 确保UE加载完成后，放入内容。
+    this.$nextTick(() => {
+      const _this = this;
+      this.editor = UM.getEditor('ueid', this.config); // 初始化UE
+      this.editor.addListener('ready', function () {
+        _this.editor.setContent(_this.defaultMsg); // 确保UE加载完成后，放入内容。
+      });
     });
   },
   methods: {

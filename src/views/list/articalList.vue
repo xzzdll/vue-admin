0b23881e7adf4393fd86c9fd4cfe35741a11d980
John <634408262@qq.com>
2018-08-21 16:24:02 +0800

    增加文章列表

--- src/views/list/articalList.vue
+++ src/views/list/articalList.vue
@@ -3,12 +3,18 @@
     <el-container>
       <el-main>
         <el-table :data="tableData">
-          <el-table-column prop="date" label="日期" min-width="140" align="center">
+          <el-table-column label="操作" :show-overflow-tooltip="true" min-width="40" align="center">
+            <template slot-scope="scope">
+              <el-button plain @click="handleEditClick(scope.row.id)" type="primary" size="mini">编辑</el-button>
+              <el-button plain @click="handleDeleteClick(scope.row.id)" type="danger" size="mini">删除</el-button>
+            </template>
           </el-table-column>
-          <el-table-column prop="name" label="姓名" min-width="120" align="center">
+          <el-table-column prop="_id" label="id" min-width="40" align="center">
           </el-table-column>
-          <el-table-column prop="address" label="地址" min-width="150" align="center">
+          <el-table-column prop="title" label="标题" min-width="40" align="center">
           </el-table-column>
+          <!-- <el-table-column prop="content" label="内容" min-width="150" align="center" show-overflow-tooltip>
+          </el-table-column> -->
         </el-table>
         <el-pagination @size-change="handleSizeChange" class="z-pagination" :current-page.sync="currentPage" :page-size="pageSize" layout="sizes, prev, pager, next, jumper,total" :total="totalRows">
         </el-pagination>
@@ -18,28 +24,36 @@
 </template>
 
 <script>
+import fetch from '../../fetch/api';
 export default {
   data () {
     return {
-      item: {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      },
+      tableData: [],
       totalRows: 60,
       currentPage: 1,
       pageSize: 20
     };
   },
   computed: {
-    tableData: function () {
-      return Array(this.pageSize).fill(this.item);
-    }
+
   },
   methods: {
     handleSizeChange (val) {
       this.pageSize = val;
     }
+  },
+  mounted () {
+    fetch('artical/list').then((data) => {
+      if (data.status === 'true') {
+        this.$message({
+          message: data.message,
+          type: 'success'
+        });
+        this.tableData = data.list;
+      } else {
+        this.$message.error(data.message);
+      }
+    });
   }
 };
 </script>

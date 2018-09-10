eb76eedeef7d64d6df8378de27ffbdb7f70d4f20
John <634408262@qq.com>
2018-09-10 15:54:16 +0800

    更改项目为相对路径

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../html'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
